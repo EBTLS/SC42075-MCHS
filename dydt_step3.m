2a95b6098245ed68e16147cb96116043c8b59626
ebtls <zjx119009@163.com>
2022-05-10 19:31:46 +0200

    finish step 2.2

---
+++ dydt_step3.m
@@ -0,0 +1,7 @@
+function dydt = dydt_step3(u,model,alpha,beta)
+%DYDT_STEP3 the function is used to continuous-time simulation of step 3
+%   
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
