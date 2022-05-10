c430ec0d30d5e714ee0714bf55932f0fd79f57c8
ebtls <zjx119009@163.com>
2022-05-10 20:13:13 +0200

    add step 2.3

--- dydt_step3.m
+++ dydt_step3.m
@@ -1,7 +1,44 @@
-function dydt = dydt_step3(u,model,alpha,beta)
+function dydt = dydt_step3(t,y,model,alpha,beta,m,gamma,b,c,vmax)
 %DYDT_STEP3 the function is used to continuous-time simulation of step 3
-%   
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%  
+% input:
+%   t,y: parameter for ode function
+%   model: whether use PWA (1) or Original Model (0)
+%   alpha, beta: parameter in the PWA
+%   m,gamma,b,c,vmax: system parameters
+
+    dydt = zeros(2,1);
+    
+    % generate a sinusoidal throttle input
+    u = sin(pi*t);
+    
+    % calculate dydt
+    if model == 0
+    % if we want to use the Original Model
+    
+        dydt(1) = y(2);
+        dydt(2) = 1/m * b/(1 + gamma * 1) * u - 1/m * c * y(2)^2; 
+        
+    elseif model ==1
+    % if we want to use the PWA Model
+        
+        if y(2) <= alpha
+        % if lies in the first part
+        
+            dydt(1) = y(2);
+            dydt(2) = 1/m * b/(1 + gamma * 1) * u - 1/m * beta/alpha * y(2);
+        
+        else
+        % if lies in the second part
+        
+            dydt(1) = y(2);
+            dydt(2) = 1/m * b/(1 + gamma * 1) * u -...
+                1/m * (c * vmax^2 - beta)/(vmax - alpha) * (x - vmax) + c * vmax^2;
+                    
+        end
+        
+    end
+
+
 end
 
